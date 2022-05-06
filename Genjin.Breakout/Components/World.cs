b1e28d955611c679500692fd4b94bdc9e350d404
Geir Sagberg <geir.sagberg@gmail.com>
2022-05-06 14:27:08 +0200

    Use a long as bitmask for component bits for ECS

--- Genjin.Breakout/Components/World.cs
+++ Genjin.Breakout/Components/World.cs
@@ -1,21 +1,21 @@
 namespace Genjin.Breakout.Components;
 
 public class World : IDrawable {
+    private readonly Dictionary<long, long> componentBitsByEntity = new();
+    private readonly Dictionary<Type, int> componentIdByType = new();
+    private readonly Dictionary<Type, Dictionary<long, Object>> componentPoolsByType = new();
+    private readonly Dictionary<long, Entity> entities = new();
+
+    private readonly List<ISystem> systems = new();
+
+    private int componentCount;
+    private long entityCount;
+
     public void Draw() {
         foreach (var drawSystem in systems.OfType<IDrawSystem>()) {
             drawSystem.Draw();
         }
     }
-
-    private readonly Dictionary<Type, Dictionary<long, Object>> componentPoolsByType = new();
-
-    // private readonly Dictionary<long, BitVector32> componentBitsByEntity = new();
-
-    private readonly Dictionary<long, Entity> entities = new();
-
-    private readonly List<ISystem> systems = new();
-
-    private long entityCount;
 
     public Entity CreateEntity() {
         var entity = new Entity(++entityCount, this);
@@ -25,8 +25,14 @@ public class World : IDrawable {
 
     private Dictionary<long, Object> GetOrCreateComponentPool(Type componentType) {
         if (!componentPoolsByType.TryGetValue(componentType, out var componentPool)) {
+            var componentId = ++componentCount;
+            if (componentId > 64) {
+                throw new Exception("Too many components");
+            }
+
             componentPool = new Dictionary<long, Object>();
             componentPoolsByType[componentType] = componentPool;
+            componentIdByType[componentType] = componentId;
         }
 
         return componentPool;
@@ -35,12 +41,16 @@ public class World : IDrawable {
     public void AddComponent<T>(long entity, T component) where T : notnull {
         var componentPool = GetOrCreateComponentPool(typeof(T));
         componentPool[entity] = component;
+        componentBitsByEntity[entity] = componentBitsByEntity.GetValueOrDefault(entity, 0) |
+            (1L << (componentIdByType[typeof(T)] - 1));
     }
 
-    public IEnumerable<Entity> GetEntitiesMatchingAll(params Type[] types) =>
-        types.Select(GetOrCreateComponentPool)
-            .Select(p => p.Keys.AsEnumerable()).Aggregate((a, b) => a.Intersect(b))
-            .Select(id => entities[id]);
+    public IEnumerable<Entity> GetEntitiesMatchingAll(params Type[] types) {
+        var bits = types.Select(type => componentIdByType.GetValueOrDefault(type))
+            .Aggregate(0L, (bits, id) => id == 0 ? bits : bits | (1L << (id - 1)));
+        return componentBitsByEntity.Where(kvp => (kvp.Value & bits) == bits)
+            .Select(kvp => entities[kvp.Key]);
+    }
 
     public T GetComponent<T>(long entity) =>
         componentPoolsByType[typeof(T)][entity] is T
